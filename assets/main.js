59a0c4670cd4caf71531390b00bd53484a1b191c
Justin Patenaude <justin@oakislanddesigns.com>
2023-05-19 10:02:40 -0500

    Contact form success message

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,26 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    }
+  }
+  return false;
+};
+
+//If the contact form sent successfully, show the "Thank you" message.
+if(getUrlParameter('form') == "success"){
+	var contactMessage = $('.contact-message');
+	contactMessage.fadeIn(300, "linear");
+	contactMessage.delay(5000).fadeOut(300, "linear");
+}
+
 $(document).ready(function(){
   //Adds smooth scroll to internal links.
 	$('a[href*="#"]:not([href="#"],.no-scroll)').click(function() {
